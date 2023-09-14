a52178746cf7cd53ff4284c496582dd92788a15b
Azi <subalt4amail@gmail.com>
2023-09-13 18:38:41 -0700

    Start development

---
+++ csc648-03-fa23-team04/src/include/navigation.js
@@ -0,0 +1,12 @@
+const NavigationBar = () => {
+    return (
+      <nav className="navbar">
+        <h1>tutorPlus++</h1>
+        <div className="links">
+          <a>About</a>
+        </div>
+      </nav>
+    );
+}
+ 
+export default NavigationBar;
---
+++ csc648-03-fa23-team04/src/pages/About.js
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+    return ( 
+        <div className="content">
+            <h1>About</h1>
+            <p>Learn about the people who designed this website here.</p>
+        </div>
+     );
+}
+ 
+export default AboutPage;
