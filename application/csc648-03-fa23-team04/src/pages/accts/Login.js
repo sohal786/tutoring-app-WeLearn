81c9f8ec2ba8a985d88a234c060d37092103a4c0
Azi <subalt4amail@gmail.com>
2023-11-09 00:25:31 -0800

    Login Form done

--- application/csc648-03-fa23-team04/src/pages/accts/Login.js
+++ application/csc648-03-fa23-team04/src/pages/accts/Login.js
@@ -1,11 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const LoginPage = () => {
   return (
     <div className="content">
-      <div className="form">
+      <div className="loginContainer">
         <h1>Hello.</h1>
+        <p>
+          Sign in using your San Francisco State University email address to get
+          started.
+        </p>
+        {/* Login form provided by react-bootstrap */}
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="Email Address" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Link to="/">
+              <a>Forgot password?</a>
+            </Link>
+
+            <br></br>
+            <br></br>
+            
+            <Button variant="primary" type="submit">
+              Log in
+            </Button>
+
+            <Link to="/register">
+              <Button variant="secondary">Sign up</Button>
+            </Link>
+          </Form.Group>
+        </Form>
       </div>
     </div>
   );
