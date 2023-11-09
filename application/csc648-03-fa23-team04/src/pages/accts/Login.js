95313bf9cc5cafae34364f1bee99236ff7e4b972
Azi <subalt4amail@gmail.com>
2023-11-09 00:38:36 -0800

    Cosmetic changes

--- application/csc648-03-fa23-team04/src/pages/accts/Login.js
+++ application/csc648-03-fa23-team04/src/pages/accts/Login.js
@@ -2,41 +2,55 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import { FloatingLabel } from "react-bootstrap";
 
 const LoginPage = () => {
   return (
     <div className="content">
       <div className="loginContainer">
         <h1>Hello.</h1>
-        <p>
-          Sign in using your San Francisco State University email address to get
-          started.
-        </p>
         {/* Login form provided by react-bootstrap */}
-        <Form>
+        <Form className="loginForm">
+          <p>
+            Sign in using your San Francisco State University email address to
+            get started.
+          </p>
+
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Email Address" />
+            <FloatingLabel
+              className="mb-3"
+              controlId="formBasicEmail"
+              label="Email Address"
+            >
+              <Form.Control type="email" placeholder="Email Address" />
+            </FloatingLabel>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Password" />
+            <FloatingLabel
+              className="mb-3"
+              controlId="formBasicPassword"
+              label="Password"
+            >
+              <Form.Control type="password" placeholder="Password" />
+            </FloatingLabel>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Link to="/">
               <a>Forgot password?</a>
             </Link>
+            <p>    </p>
+            <Link to="/register">
+              <a>Sign Up </a>
+            </Link>
 
             <br></br>
             <br></br>
-            
+
             <Button variant="primary" type="submit">
               Log in
             </Button>
-
-            <Link to="/register">
-              <Button variant="secondary">Sign up</Button>
-            </Link>
           </Form.Group>
         </Form>
       </div>
