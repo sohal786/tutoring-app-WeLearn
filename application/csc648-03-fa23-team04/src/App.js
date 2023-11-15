a0cb2e69206adda6f765bc5109a4f738463bdd29
Carcris23 <Jorgepermora@gmail.com>
2023-11-14 17:04:24 -0800

    Revert "Merge branch 'search_bar' of https://github.com/CSC-648-SFSU/csc648-03-fa23-team04 into search_bar"
    This reverts commit c3da2ffa7b536e18f88cc0d28117167a18c00e07, reversing
    changes made to cb9dbf4525e6c64c6bb8215ce0373cc9579801e5.

---
+++ application/csc648-03-fa23-team04/src/App.js
@@ -0,0 +1,49 @@
+
+import "./css/global.css";
+
+import NavigationBar from "./include/navigation";
+
+import HomePage from "./pages/Home";
+import AboutPage from "./pages/About";
+import NotFound from "./include/NotFound";
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+//React Router
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Akshat from "./pages/akshat";
+import Aakanksha from "./pages/aakanksha";
+import Andy from "./pages/andy";
+import Jorge from "./pages/jorge";
+import Azi from "./pages/azi";
+import Charter from "./pages/charter";
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavigationBar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="akshat" element={<Akshat />} />
+            <Route path="aakanksha" element={<Aakanksha/>} />
+            <Route path="andy" element={<Andy />} />
+            <Route path="jorge" element={<Jorge />} />
+            <Route path="azi" element={<Azi />} />
+            <Route path="charter" element={<Charter />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
