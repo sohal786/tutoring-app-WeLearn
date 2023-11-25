51395d414e9a5b75f8efb6b62b70aca74839fb02
Azi <subalt4amail@gmail.com>
2023-11-25 15:51:51 -0800

    Create basic layout for dashboard

--- application/client/src/pages/Dashboard.js
+++ application/client/src/pages/Dashboard.js
@@ -1,10 +1,43 @@
-const DashboardHome = () => {
-    return ( 
-        <div className="content">
-            <h1>Hello, [username]</h1>
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button, Card } from "react-bootstrap";
+import placeholderFox from "../img/placeholder.png"
 
+const DashboardHome = () => {
+    return (
+      <div className="content">
+        <h1>Hello, [username]</h1>
+        <br></br>
+        <div className="profileArea">
+          <Card>
+            <Card.Body>
+              <Card.Title>User</Card.Title>
+              <div className="userInformationArea">
+                <img class="profileImage" src={placeholderFox}></img>
+              </div>
+            </Card.Body>
+          </Card>
         </div>
-     );
+        <br></br>
+        <div className="contactsArea">
+          <Card>
+            <Card.Body>
+              <Card.Title>Recently Contacted</Card.Title>
+              Recently Contacted
+            </Card.Body>
+          </Card>
+        </div>
+        <br></br>
+        <div className="messagesArea">
+          <Card>
+            <Card.Body>
+              <Card.Title>Messages</Card.Title>
+              Message content
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    );
 }
  
 export default DashboardHome;
