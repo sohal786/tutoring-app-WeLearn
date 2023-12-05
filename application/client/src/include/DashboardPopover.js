9f8063a002c41575ab496067d24350b74d44c98c
Azi <subalt4amail@gmail.com>
2023-12-05 13:24:19 -0800

    Adjust font sizing

--- application/client/src/include/DashboardPopover.js
+++ application/client/src/include/DashboardPopover.js
@@ -3,41 +3,41 @@ import placeholderFox from "../img/placeholder.png";
 
 const DashboardPopover = () => {
     return (
-        <OverlayTrigger
-          trigger="click"
-          placement="bottom"
-          overlay={
-            <Popover id="popover-positioned-bottom center">
-              <Popover.Header>
-                <div className="profileArea text-center justify-content-md-center">
-                  <img src={placeholderFox} className="profileImage"></img>
-                  <h2>Username</h2>
-                </div>
-              </Popover.Header>
-              <Popover.Body>
-                  <h3>Messages</h3>
+      <OverlayTrigger
+        trigger="click"
+        placement="bottom"
+        overlay={
+          <Popover id="popover-positioned-bottom center">
+            <Popover.Header>
+              <div className="profileArea text-center justify-content-md-center">
+                <img src={placeholderFox} className="profileImage"></img>
+                <h2>Username</h2>
+              </div>
+            </Popover.Header>
+            <Popover.Body>
+              <h3>Messages</h3>
 
-                    {/* Map messages belong here */}
-                    <div className="messageContainer">
-                      <Card>
-                        <Card.Body>
-                          <h4>Recipient Name</h4>
-                          <p>Message Content</p>
-                        </Card.Body>
-                      </Card>
+              {/* Map messages belong here */}
+              <div className="messageContainer">
+                <Card>
+                  <Card.Body>
+                    <strong>Recipient Name</strong>
+                    <p>Message Content</p>
+                  </Card.Body>
+                </Card>
 
-                      <Card>
-                        <Card.Body>
-                          <h4>Recipient Name</h4>
-                          <p>Message Content</p>
-                        </Card.Body>
-                      </Card>
-                    </div>
-              </Popover.Body>
-            </Popover>
-          }>
-          <a href="#">Open menu</a>
-        </OverlayTrigger>
+                <Card>
+                  <Card.Body>
+                    <strong>Recipient Name</strong>
+                    <p>Message Content</p>
+                  </Card.Body>
+                </Card>
+              </div>
+            </Popover.Body>
+          </Popover>
+        }>
+        <a href="#">Open menu</a>
+      </OverlayTrigger>
     );
 }
  
